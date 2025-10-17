361047f41d6e09128e9549be0731e8591c1d01c3
OldM1lk <mrlaimwin@gmail.com>
2025-10-17 20:17:07 +0700

    Add ability to delete task

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const inputDescription = form.querySelector('input[name="description"]');
 const tasksList = document.querySelector('.tasks__list');
 const emptyText = document.querySelector('.tasks__empty');
 const taskTemplate = document.getElementById('task-template');
+const alert = document.querySelector('.alert');
 
 function init() {
     renderTasks();
@@ -57,9 +58,15 @@ function createTaskElement(task) {
 
     const titleEl = el.querySelector('.text__title');
     const descEl = el.querySelector('.text__description');
+    const deleteButton = el.querySelector('.button-delete');
 
     titleEl.textContent = task.title;
     descEl.textContent = task.description;
+
+    deleteButton.addEventListener('click', e => {
+        e.stopPropagation();
+        openAlert(task.id);
+    })
 
     const taskItem = el.querySelector('.task__content');
     const taskTools = el.querySelector('.task__tools');
@@ -70,6 +77,28 @@ function createTaskElement(task) {
 
 function toggleTaskTools(taskTools) {
     taskTools.classList.toggle('hidden');
+}
+
+function deleteTask(id) {
+    tasks = tasks.filter(t => t.id !== id);
+    renderTasks();
+}
+
+function closeWindow(window) {
+    window.classList.add('hidden');
+}
+
+function openAlert(id) {
+    alert.classList.remove('hidden');
+
+    const confirmButton = alert.querySelector('.button-confirm');
+    const cancelButton = alert.querySelector('.button-cancel');
+
+    confirmButton.onclick = () => {
+        deleteTask(id);
+        closeWindow(alert);
+    };
+    cancelButton.onclick = () => closeWindow(alert);
 }
 
 document.addEventListener('DOMContentLoaded', init);
